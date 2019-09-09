74a8f178d1b76abf5a1e5ed371a7ed63347087c7
Terique915 <54835492+Terique915@users.noreply.github.com>
2019-09-09 09:28:12 +0200

    Hoofdstuk 1 2 en 4.1 en 4.2

--- src/H04/Opdracht42.java
+++ src/H04/Opdracht42.java
@@ -13,7 +13,10 @@ public class Opdracht42 extends Applet {
 
     public void paint(Graphics g) {
         g.drawRect(100,300,280,160);
-        g.drawLine(100,300,300,155);
-        g.drawLine(300,155,380,300);
+       g.drawLine(100,300,230,200);
+       g.drawLine(230,200,380,300);
+       g.drawRect(300,330,50,60);
+       g.drawRect(200,370,50,90);
     }
+
 }
