fe061b5135d77e4845899f469032e063065250ce
Terique915 <54835492+Terique915@users.noreply.github.com>
2019-09-13 13:26:33 +0200

    Hoofdstuk 1 2 4 5 6 7 klaar

---
+++ src/H07/Opdracht71.java
@@ -0,0 +1,13 @@
+package H07;
+
+//Vraag 1:Kun je in de Java API (zie web-links) deze methode ergens vinden?
+//Antwoord: Bij methode details staat resize. De ovride zegt cresize in class Components en parameters  width en height.
+//          Dit verandert hoe groot of klein de Applet word.
+//Vraag 2:In welke klasse wordt deze methode gedefinieerd?
+//Antwoord: java.awt.component
+//Vraag 3:Dezelfde klasse heeft een tweede methode met dezelfde naam. Wat is het verschil?
+//Antwoord: een verandert een groot of klein de applet zal zijn , de andere verandert dingen met een nieuwe width en height.
+
+
+public class Opdracht71 {
+}
